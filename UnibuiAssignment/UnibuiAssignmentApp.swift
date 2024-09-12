458496e3e35e7a8b1ff2f48a731b66b79402fb3d
Yan Brunshteyn <brunshte@gmail.com>
2024-09-12 09:55:09 -0700

    Initial Commit

---
+++ UnibuiAssignment/UnibuiAssignmentApp.swift
@@ -0,0 +1,20 @@
+//
+//  UnibuiAssignmentApp.swift
+//  UnibuiAssignment
+//
+//  Created by admin on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UnibuiAssignmentApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
