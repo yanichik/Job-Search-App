f97cd30143d40e358b3296dfab0a797ccb155056
Yan Brunshteyn <brunshte@gmail.com>
2024-09-19 14:34:40 -0700

    content view

---
+++ UnibuiAssignment/Views/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  UnibuiAssignment
+//
+//  Created by admin on 9/12/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @ObservedObject var csvDataManager = CSVDataManager()
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+    
+    var body: some View {
+        NavigationStack {
+            List(csvDataManager.jobs) { job in
+                NavigationLink(destination: DetailView(job)) {
+                    HStack {
+                        Text(job.jobTitle)
+                        Text(job.companyName)
+                        Text(job.location)
+                    }
+                }.navigationTitle("Open Positions")
+            }
+        }.onAppear(perform: {
+            csvDataManager.loadJobs(from: "jobsMock")
+        })
+        //        NavigationView {
+        //            List {
+        //                ForEach(items) { item in
+        //                    NavigationLink {
+        //                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        //                    } label: {
+        //                        Text(item.timestamp!, formatter: itemFormatter)
+        //                    }
+        //                }
+        //                .onDelete(perform: deleteItems)
+        //            }
+        //            .toolbar {
+        //                ToolbarItem(placement: .navigationBarTrailing) {
+        //                    EditButton()
+        //                }
+        //                ToolbarItem {
+        //                    Button(action: addItem) {
+        //                        Label("Add Item", systemImage: "plus")
+        //                    }
+        //                }
+        //            }
+        //            Text("Select an item")
+        //        }
+    }
+    
+//    let jobs = [
+//        Job(jobTitle: "janitor", companyName: "Clean Me", location: "San Francisco", jobDescription: "clean my shit", requirements: "must be thorough"),
+//        Job(jobTitle: "SWD", companyName: "CodingAI", location: "San Jose", jobDescription: "program me a robot", requirements: "never sleep biatch!")
+//    ]
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
